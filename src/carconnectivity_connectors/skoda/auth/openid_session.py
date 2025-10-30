7929cf6808bcae02796393159a24cdecec6d4af6
Till Steinbach <tillsteinbach@users.noreply.github.com>
2025-10-30 15:48:09 +0100

    Fix typo

--- src/carconnectivity_connectors/skoda/auth/openid_session.py
+++ src/carconnectivity_connectors/skoda/auth/openid_session.py
@@ -1,4 +1,4 @@
- """Implements a session class that handles OpenID authentication."""
+"""Implements a session class that handles OpenID authentication."""
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
